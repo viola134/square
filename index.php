f76021d8dc4a228bc4dd6f7117747ea47d4ae0e6
viola134 <113242400+viola134@users.noreply.github.com>
2022-09-17 20:01:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ $r = (rand(1, 20));
 const pi = 3.14;
 $A = pi*$r*$r;
 echo ((" Радиус круга:" . $r). " Площадь круга:" . $A );
-echo "(<div style= \"color:red; width: $r*2; height: $r*2; border-box: 50%;\"> </div>)";
+echo "(<div style= \"color:red; width: $r*2px; height: $r*2px; border: solid 1px; border-box: 50%;\"> </div>)";
